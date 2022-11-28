ae3d61a8220ff73600cf1c315cdc4090001604c9
Farkas Valter <walter20020110@gmail.com>
2022-11-28 07:50:13 +0100

    relative database path and some cleanup

--- Csaladi_Fotoalbum/DatabaseModels/DatabaseContext.cs
+++ Csaladi_Fotoalbum/DatabaseModels/DatabaseContext.cs
@@ -27,7 +27,7 @@ public partial class DatabaseContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\walte\\source\\repos\\Csaladi_Fotoalbum\\Csaladi_Fotoalbum\\DATABASE.mdf;Integrated Security=True");
+        => optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename="+Path.GetFullPath("DATABASE.mdf")+";Integrated Security=True");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
